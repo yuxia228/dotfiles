d7b9beedebdb9abf88b9711874fe5434c6882360
yuxia228 <yuxia228@gmail.com>
2022-06-28 13:17:45 +0900

    init.sh: Add some git configs
    Signed-off-by: yuxia228 <yuxia228@gmail.com>

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ SCRIPT_DIR=$(cd `dirname $0`; pwd)
 cd ${SCRIPT_DIR}
 LN_DIR_OPT=${LN_DIR_OPT:-"-sfd"}
 
+####################################################
+# functions                                        #
+####################################################
 Usage () {
     echo "Usage:"
     echo "    $0"
@@ -14,13 +17,28 @@ Usage () {
 }
 
 git_config () {
+    # basic config
+    git config --global color.ui true
+    git config --global color.diff auto
+    git config --global color.status auto
+    git config --global color.branch auto
     git config --global core.hooksPath ${HOME}/.githooks
     git config --global core.editor vim
     git config --global commit.verbose true
     git config --global pull.ff only
-    git config --global alias.tree "log --graph --all \
-        --format='%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s'"
+    git config --global diff.algorithm histogram
+    git config --global push.default simple
+    # aliases
+    git config --global alias.tree "log --graph --all --format='%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s'"
+    git config --global alias.squash "merge --squash --ff"
+    git config --global alias.difff "diff --word-diff"
+    git config --global alias.mkpatch "format-patch --subject-prefix=PATCH -o ./"
+    git config --global alias.mkpatches "mkpatch --cover-letter"
 }
+
+####################################################
+# main process                                     #
+####################################################
 
 if [[ "$(echo $*|grep -e --help -e -h)" != "" ]]; then
     Usage; exit;
