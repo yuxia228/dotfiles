8683b2f4602b25f46d73f304b6e8f8f4fccd72df
yuxia228 <yuxia228@gmail.com>
2024-02-05 11:32:47 +0900

    windows: Update each file
    Signed-off-by: yuxia228 <yuxia228@gmail.com>

--- init.bat
+++ init.bat
@@ -5,6 +5,7 @@ cd /d %~dp0
 REM alacritty
 mkdir %HOMEPATH%\AppData\Roaming\alacritty\
 copy /Y .\alacritty.yml %HOMEPATH%\AppData\Roaming\alacritty\alacritty.yml
-mkdir %HOMEPATH%\scoop\apps\wezterm\current\
-copy /Y .\wezterm.lua %HOMEPATH%\scoop\apps\wezterm\current\wezterm.lua
+copy /Y .\alacritty.toml %HOMEPATH%\AppData\Roaming\alacritty\alacritty.toml
+mkdir %HOMEPATH%\.config\wezterm
+copy /Y .\wezterm.lua %HOMEPATH%\.config\wezterm\wezterm.lua
 
